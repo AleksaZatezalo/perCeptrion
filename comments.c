70f03d3f335628e0d7e658279be24c668b276764
aleksa <zabumaphu@gmail.com>
2024-08-11 23:09:02 -0400

    done comments.

--- comments.c
+++ comments.c
@@ -6,8 +6,11 @@ int main()
 /*
 Prints 10 stars
 */
-for (int i = 0; i < 10; i++){
-    printf("*");
+for (int i = 0; i < 10; i++){ // i = rows, j = cols
+    for (int j=0; j < 10; j++){
+        printf("* ");    
+    }
+    printf("\n");
     }
 
 return 0;
