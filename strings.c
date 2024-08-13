8917efe6257f5e26f7ee611aef067bc2f102f5bd
aleksa <zabumaphu@gmail.com>
2024-08-12 21:03:48 -0400

    strings done.

--- strings.c
+++ strings.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-    char array[] = "My String";
-    char *str = "New String";
-    printf("%s", str);
+    char *str1 = "My String";
+    char *str2 = "New String";
+    if (strcmp(str1, str2)) {
+        printf("false");
+    }
     return 0;
 }
