9cefadef503b6600789e1fbaf1daf6f90429f77c
aleksa <zabumaphu@gmail.com>
2024-08-14 17:46:18 -0400

    File error checking done.

--- files.c
+++ files.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define BUFFER_SIZE 1024
 
@@ -12,11 +13,31 @@ int main(int argc, char *argv[])
     }
 
     int fd1 = open(argv[1], O_RDONLY);
-    int fd2 = open(argv[2], O_WRONLY | O_CREAT);
+    if (fd1 == -1){
+        fprintf(stderr, "ERROR: Failed to open file.");
+        return -1;
+ 
+    }
+
+    int fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC);
+    if (fd2 == -1){
+        fprintf(stderr, "ERROR: Failed to create file.");
+        return -1;
+    }
+    
+    
     char buff[BUFFER_SIZE];
     int bytes;
 
     while ((bytes = read(fd1, buff, BUFFER_SIZE)) !=0) {
+        if (bytes == -1) {
+            if(errno == EINTR){
+                continue;
+            } else {
+                fprint(stderr, "\nERROR: Read failed.");
+                break;
+            }
+        }
         for (int i = 0; i < bytes; i++) {
             write(fd2, buff, bytes);
         }
