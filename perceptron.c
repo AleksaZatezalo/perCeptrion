fcbc0d5522abe06d53979bc9270d947dbb261241
aleksa <zabumaphu@gmail.com>
2024-08-14 23:36:58 -0400

    Perceptor started.

---
+++ perceptron.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    float p;
+    float treshold = 1.5;
+    
+    float x1;
+    float w1;
+
+    float x2;
+    float w2;
+
+    float x3;
+    float w3;
+
+    float x4;
+    float w4;
+
+    float x5;
+    float w5;
+}
